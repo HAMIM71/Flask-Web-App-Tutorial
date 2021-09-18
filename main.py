66b3246779ff242af964fb74426440e6fdd61ae6
Hamim Yasin <hamimyasin71@gmail.com>
2021-09-18 07:30:12 +0000

    .

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 from website import create_app
-import pyrebase
+
 
 app = create_app()
 
-config = {
-    'apiKey': "AIzaSyAdNWl10pzwhawDFR-pKvXgJ_jz24j9pHI",
-    'authDomain': "space-667e4.firebaseapp.com",
-    'projectId': "space-667e4",
-    'storageBucket': "space-667e4.appspot.com",
-    'messagingSenderId' : "403454559792",
-    'appId' : "1:403454559792:web:7f73a741c1ba2b0c194ff5",
-    'measurementId' : "G-W0P1BRD9G0"
-}
 
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
 
 
 if __name__ == '__main__':
